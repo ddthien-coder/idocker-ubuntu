3bd27f3a5e200554b6ff1941036043e96f3b5a48
ddthien <55986641+ddthien-coder@users.noreply.github.com>
2026-03-06 13:23:07 +0700

    Install Docker Compose and check version
    Add installation steps for Docker Compose.

--- IDOU.sh
+++ IDOU.sh
@@ -10,5 +10,10 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 docker --version
 
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
 sudo systemctl start docker
 sudo systemctl enable docker
